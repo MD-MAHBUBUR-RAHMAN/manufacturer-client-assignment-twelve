3b0f3b4ff209469a3c320d1f05772151c16f5e18
MD-MAHBUBUR-RAHMAN <nirjhor211288@gmapl.com>
2022-05-22 05:07:37 +0600

    BusinessSummery of home page added

---
+++ src/pages/Home/BusinessSummery.js
@@ -0,0 +1,76 @@
+import React from "react";
+// import { BeakerIcon } from "@heroicons/react/solid";
+
+const BusinessSummery = () => {
+  return (
+    <>
+      <h1 className="text-center text-xl font-semibold md:text-5xl text-primary my-10">
+        Our Business Summery
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 my-10">
+        <div className="stat shadow-lg">
+          <div className="stat-figure text-secondary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-10 w-10"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z" />
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          <div className="stat-title text-xl font-bold">Revinue</div>
+          <div className="stat-value">31K</div>
+          <div className="stat-desc font-bold">
+            Jan 1<sup>st</sup> - May 31<sup>st</sup>
+          </div>
+        </div>
+
+        <div className="stat shadow-lg">
+          <div className="stat-figure text-secondary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-10 w-10"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
+            </svg>
+          </div>
+          <div>
+            <div className="stat-title text-xl font-bold">Customers</div>
+            <div className="stat-value">4,200</div>
+            <div className="stat-desc font-bold">↗︎ 400 (22%)</div>
+          </div>
+        </div>
+
+        <div className="stat shadow-lg">
+          <div className="stat-figure text-secondary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5 2a1 1 0 011 1v1h1a1 1 0 010 2H6v1a1 1 0 01-2 0V6H3a1 1 0 010-2h1V3a1 1 0 011-1zm0 10a1 1 0 011 1v1h1a1 1 0 110 2H6v1a1 1 0 11-2 0v-1H3a1 1 0 110-2h1v-1a1 1 0 011-1zM12 2a1 1 0 01.967.744L14.146 7.2 17.5 9.134a1 1 0 010 1.732l-3.354 1.935-1.18 4.455a1 1 0 01-1.933 0L9.854 12.8 6.5 10.866a1 1 0 010-1.732l3.354-1.935 1.18-4.455A1 1 0 0112 2z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          <div className="stat-title text-xl font-bold">Review</div>
+          <div className="stat-value">1,200</div>
+          <div className="stat-desc  font-bold">↘︎ 90 (14%)</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BusinessSummery;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "../Shared/Footer";
 import Banner from "./Banner";
+import BusinessSummery from "./BusinessSummery";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <BusinessSummery />
       <Footer />
     </div>
   );
