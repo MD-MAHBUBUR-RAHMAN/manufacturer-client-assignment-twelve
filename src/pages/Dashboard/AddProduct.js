baa2eebfea15debe334b64aa5c7800fc017cd6c5
MD-MAHBUBUR-RAHMAN <nirjhor211288@gmapl.com>
2022-05-25 19:19:00 +0600

    BlogPage and Add product form created

--- src/pages/Dashboard/AddProduct.js
+++ src/pages/Dashboard/AddProduct.js
@@ -1,9 +1,77 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const AddProduct = () => {
+  const { register, handleSubmit, reset } = useForm();
+  const onSubmit = (data) => {
+    // // console.log(data);
+    // const url = `https://frozen-crag-58968.herokuapp.com/product`;
+    // fetch(url, {
+    //   method: "PUT",
+    //   headers: {
+    //     "content-type": "application/json",
+    //   },
+    //   body: JSON.stringify(data),
+    // })
+    //   .then((res) => res.json())
+    //   .then((result) => {
+    //     console.log(result);
+    //     reset();
+    //   });
+    console.log(data);
+  };
   return (
     <div>
       <h3 className="text-center text-primary">Add Product</h3>
+      <form
+        className="flex flex-col border p-5 rounded-lg mx-2 my-10"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <input
+          className="mb-2 text-center shadow block my-5 rounded-lg w-full md:text-xl  p-2"
+          placeholder="Product Name"
+          {...register("name", { required: true })}
+        />
+        <input
+          className="mb-2 text-center shadow block my-5 rounded-lg w-full md:text-xl  p-2"
+          placeholder="Your Email"
+          {...register("email", { required: true })}
+        />
+        <textarea
+          className="mb-2 shadow block my-5 rounded-lg w-full md:text-xl text-center p-2"
+          placeholder="Add Detail Your Product"
+          {...register("detail", { required: true })}
+        />
+
+        <input
+          className="mb-2 shadow block my-5 rounded-lg w-full md:text-xl text-center p-2"
+          placeholder="Price Per Unit"
+          type="number"
+          {...register("price", { required: true })}
+        />
+        <input
+          className="mb-2 shadow block my-5 rounded-lg w-full md:text-xl text-center p-2"
+          placeholder="Image Url"
+          type="text"
+          {...register("img", { required: true })}
+        />
+        <input
+          className="mb-2 shadow block my-5 rounded-lg w-full md:text-xl text-center p-2"
+          placeholder="Add Quentity"
+          type="number"
+          {...register("quantity", { required: true })}
+        />
+        <input
+          className="mb-2 shadow block my-5 rounded-lg w-full md:text-xl text-center p-2"
+          placeholder="Minimum Order Quantity"
+          type="number"
+          {...register("minimum", { required: true })}
+        />
+        <input
+          className="block mx-auto bg-slate-700 px-5 py-3 mt-10 mb-5 rounded-md hover:text-blue-400 md:w-36 border-none text-red-400 md:font-semibold "
+          type="submit"
+        />
+      </form>
     </div>
   );
 };
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -3,8 +3,9 @@ import { Link, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
   return (
-    <div>
-      <div className="drawer-content flex flex-col items-end">
+    <div className="drawer drawer-mobile">
+      <input id="dashboard-drawer" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content">
         {/* <!-- Page content here --> */}
         <label
           htmlFor="dashboard-drawer"
@@ -12,43 +13,36 @@ const Dashboard = () => {
         >
           Open drawer
         </label>
+        <h2 className="text-center text-primary">Welcome To DashBoard</h2>
+        <Outlet></Outlet>
       </div>
-      <h2 className="text-center text-primary">Welcome To DashBoard</h2>
-      <Outlet></Outlet>
-      <div className="drawer drawer-mobile">
-        <input
-          id="dashboard-drawer"
-          type="checkbox"
-          className="drawer-toggle"
-        />
 
-        <div className="drawer-side">
-          <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
-          <ul className="menu p-4 overflow-y-auto w-48 bg-base-100 text-base-content">
-            {/* <!-- Sidebar content here --> */}
-            <li>
-              <Link to="/dashboard">My Orders</Link>
-            </li>
-            <li>
-              <Link to="/dashboard/addreview">Add Review</Link>
-            </li>
-            <li>
-              <Link to="/dashboard/myprofile">MY Profile</Link>
-            </li>
-            <li>
-              <Link to="/dashboard/manageorder">Manage Order</Link>
-            </li>
-            <li>
-              <Link to="/dashboard/addproduct">Add Product</Link>
-            </li>
-            <li>
-              <Link to="/dashboard/makeadmin">Make Admin</Link>
-            </li>
-            <li>
-              <Link to="/dashboard/manageproduct">Manage Product</Link>
-            </li>
-          </ul>
-        </div>
+      <div className="drawer-side bg-slate-200">
+        <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
+        <ul className="menu p-4 overflow-y-auto w-48 bg-base-100 text-base-content">
+          {/* <!-- Sidebar content here --> */}
+          <li>
+            <Link to="/dashboard">My Orders</Link>
+          </li>
+          <li>
+            <Link to="/dashboard/addreview">Add Review</Link>
+          </li>
+          <li>
+            <Link to="/dashboard/myprofile">MY Profile</Link>
+          </li>
+          <li>
+            <Link to="/dashboard/manageorder">Manage Order</Link>
+          </li>
+          <li>
+            <Link to="/dashboard/addproduct">Add Product</Link>
+          </li>
+          <li>
+            <Link to="/dashboard/makeadmin">Make Admin</Link>
+          </li>
+          <li>
+            <Link to="/dashboard/manageproduct">Manage Product</Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
